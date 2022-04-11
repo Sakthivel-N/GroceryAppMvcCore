c79848804bc4dbb4e3249fbffc941d7902aadc86
Ragul Raj A <ragulraj.a2018@gmail.com>
2022-04-11 22:49:05 +0530

    Admin,Cart Added

---
+++ GroceryAppMvcCore/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceryAppMvcCore.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        [Required,MinLength(5),MaxLength(20)]
+        public string AdminName { get; set; }
+        [Required, MinLength(5), MaxLength(20)]
+        public string EmailId { get; set; }
+        [Required, MinLength(5), MaxLength(20)]
+        public string Password { get; set; }
+        
+
+    }
+}
