2e9ae90aa22d21910d15ced8f11b61dc51eded1d
Sakthivel-N <sakthiwebdev3520@gmail.com>
2022-04-12 11:17:10 +0530

    package added and Program.cs Session Added

--- GroceryAppMvcCore/Program.cs
+++ GroceryAppMvcCore/Program.cs
@@ -1,7 +1,12 @@
 
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -17,7 +22,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
