69b5cce166fee8846487447990a2c4d22e3a0973
Balakrishnan N <60445196+bkrish1719205@users.noreply.github.com>
2022-04-12 09:57:45 +0530

    Feedback Model Added

---
+++ GroceryAppMvcCore/Models/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GroceryAppMvcCore.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int FeedbackID { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public virtual User Users { get; set; }
+
+        [MaxLength(255)]
+        public string FeedbackMsg { get; set; }
+    }
+}
