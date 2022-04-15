7d5564736f55df0221d530a266238ddede88ab9a
Balakrishnan N <60445196+bkrish1719205@users.noreply.github.com>
2022-04-15 11:00:01 +0530

    ViewOrders Added

--- GroceryAppMvcCore/Controllers/UsersController.cs
+++ GroceryAppMvcCore/Controllers/UsersController.cs
@@ -161,5 +161,34 @@ namespace GroceryAppMvcCore.Controllers
             }
             return RedirectToAction("Index","Home");
         }
+        public async Task<List<Order>> GetOrderView()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            HttpClient client = new HttpClient(clientHandler);
+
+            string JsonStr = await client.GetStringAsync(baseURL + "/api/Orders");
+            List<Order> result = JsonConvert.DeserializeObject<List<Order>>(JsonStr);
+            return result;
+        }
+
+        public async Task<List<Cart>> GetCartView()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            HttpClient client = new HttpClient(clientHandler);
+
+            string JsonStr = await client.GetStringAsync(baseURL + "/api/Carts");
+            List<Cart> result = JsonConvert.DeserializeObject<List<Cart>>(JsonStr);
+            return result;
+        }
+
+        public async Task<IActionResult> ViewOrders()
+        {
+            OrderView orderView = new OrderView();
+            orderView.Orders = await GetOrderView();
+            orderView.Carts = await GetCartView();
+            return View(orderView);
+
+        }
+
     }
 }
