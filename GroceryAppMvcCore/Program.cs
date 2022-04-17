33f8d5096a82c176e0ccd375e4bebb622f2052a4
Sakthi Vel <71425114+Sakthivel-N@users.noreply.github.com>
2022-04-17 20:47:35 +0530

    Our jobs

--- GroceryAppMvcCore/Program.cs
+++ GroceryAppMvcCore/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Users}/{action=ViewProducts}");
+    pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
