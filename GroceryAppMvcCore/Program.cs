afdd3b761c186cb8aea9ebff2215098c3315fbac
Sakthi Vel <71425114+Sakthivel-N@users.noreply.github.com>
2022-04-17 20:48:24 +0530

    Revert "Our jobs"
    This reverts commit 33f8d5096a82c176e0ccd375e4bebb622f2052a4.

--- GroceryAppMvcCore/Program.cs
+++ GroceryAppMvcCore/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}");
+    pattern: "{controller=Users}/{action=ViewProducts}");
 
 app.Run();
