1b73ed99d704fb9c4ecca996950a2b122ac2685c
Sakthi Vel <71425114+Sakthivel-N@users.noreply.github.com>
2022-04-17 11:22:51 +0530

    Delivery model- pickup,delivery date added

--- GroceryAppMvcCore/Controllers/AdminsController.cs
+++ GroceryAppMvcCore/Controllers/AdminsController.cs
@@ -7,13 +7,15 @@ namespace GroceryAppMvcCore.Controllers
 {
     public class AdminsController : Controller
     {
-            //nav=items
-                    //1.Index
-                    //2.ViewCustomers
-                    //3.ViewProducts
-                    //4.AddProduct
-                    //5.Feedbacks
-                    //6.Logout
+        //nav=items
+        //1.Index
+        //2.ViewCustomers
+        //3.ViewProducts
+        //4.AddProduct
+        //5.ReorderNow
+        //6.Employee
+        //5.Feedbacks
+        //6.Logout
 
 
         //API URL ADDED
--- GroceryAppMvcCore/Models/Delivery.cs
+++ GroceryAppMvcCore/Models/Delivery.cs
@@ -9,9 +9,16 @@ namespace GroceryAppMvcCore.Models
         [Required]
         public int EmployeeId { get; set; }
         public virtual Employee Employee { get; set; }
+
+        
         [Required]
         public int OrderId { get; set; }
         public virtual Order Order { get; set; }
+
+        [Required, MaxLength(13)]
+        public string PickupDate { get; set; }
+        [Required, MaxLength(13)]
+        public string DeliveryDate { get; set; }
 
         [Required]
         public bool Status { get; set; }
