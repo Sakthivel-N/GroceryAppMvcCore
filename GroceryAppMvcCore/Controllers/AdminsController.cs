1af89c1ccc24283d3804f3c3eea48cb1d6a2c7c8
Sakthi Vel <71425114+Sakthivel-N@users.noreply.github.com>
2022-04-18 18:55:23 +0530

    View Products edited

--- GroceryAppMvcCore/Controllers/AdminsController.cs
+++ GroceryAppMvcCore/Controllers/AdminsController.cs
@@ -158,7 +158,7 @@ namespace GroceryAppMvcCore.Controllers
                 string apiResponse = await response.Content.ReadAsStringAsync();
 
                 if (apiResponse != null)
-                    return RedirectToAction("Index");
+                    return RedirectToAction("ViewProducts");
                 else
                     return View();
             }
