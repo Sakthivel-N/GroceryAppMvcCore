5e3e139e77d3f3ddb0604a866793a1996f988d7c
Balakrishnan N <60445196+bkrish1719205@users.noreply.github.com>
2022-04-18 12:54:56 +0530

    Add files via upload
    Delivery View Added

---
+++ GroceryAppMvcCore/Models/DeliveryVuew.cs
@@ -0,0 +1,9 @@
+﻿namespace GroceryAppMvcCore.Models
+{
+    public class DeliveryVuew
+    {
+        public List<Order> Orders { get; set; }
+       
+        public List<Delivery> Deliveries { get; set; }
+    }
+}
