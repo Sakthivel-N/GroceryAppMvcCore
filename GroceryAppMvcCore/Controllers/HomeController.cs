1a0b39a36efb1910c87c02680097e99e2754ef5b
Sakthi Vel <71425114+Sakthivel-N@users.noreply.github.com>
2022-04-21 16:08:23 +0530

    Email changed in login

--- GroceryAppMvcCore/Controllers/HomeController.cs
+++ GroceryAppMvcCore/Controllers/HomeController.cs
@@ -45,9 +45,9 @@ namespace GroceryAppMvcCore.Controllers
         [HttpPost]
         public async Task<IActionResult> AdminLogin(Admin admin)
         {
-            if(admin.AdminName != null && admin.Password != null)
+            if(admin.EmailId != null && admin.Password != null)
             {
-                Admin admins = await GetValidAdmin(admin.AdminName, admin.Password);
+                Admin admins = await GetValidAdmin(admin.EmailId, admin.Password);
                 if(admins != null)
                 {
                     HttpContext.Session.SetString("AdminName", admins.AdminName);
@@ -95,7 +95,7 @@ namespace GroceryAppMvcCore.Controllers
 
 
         [HttpGet]
-        public async Task<Admin> GetValidAdmin(string AdminName, string Password)
+        public async Task<Admin> GetValidAdmin(string EmailId, string Password)
         {
 
             List<Admin> admin = new List<Admin>();
@@ -108,7 +108,7 @@ namespace GroceryAppMvcCore.Controllers
                     admin = JsonConvert.DeserializeObject<List<Admin>>(apiResponse);
                 }
             }
-            return (admin.FirstOrDefault(m => m.AdminName == AdminName && m.Password == Password));
+            return (admin.FirstOrDefault(m => m.EmailId == EmailId && m.Password == Password));
 
         }
 
@@ -216,9 +216,9 @@ namespace GroceryAppMvcCore.Controllers
         public async Task<IActionResult> EmployeeLogin(Employee employee)
         {
             Employee employees = new Employee();
-            if (employee.EmployeeName != null && employee.Password != null)
+            if (employee.EmaiLId != null && employee.Password != null)
             {
-                employees = await GetValidEmployee(employee.EmployeeName, employee.Password);
+                employees = await GetValidEmployee(employee.EmaiLId, employee.Password);
                 if (employees != null)
                 {
                     
@@ -239,7 +239,7 @@ namespace GroceryAppMvcCore.Controllers
         }
 
         [HttpGet]
-        public async Task<Employee> GetValidEmployee(string EmployeeName, string Password)
+        public async Task<Employee> GetValidEmployee(string EmailId, string Password)
         {
 
             List<Employee> employee = new List<Employee>();
@@ -252,7 +252,7 @@ namespace GroceryAppMvcCore.Controllers
                     employee = JsonConvert.DeserializeObject<List<Employee>>(apiResponse);
                 }
             }
-            return (employee.FirstOrDefault(m => m.EmployeeName == EmployeeName && m.Password == Password));
+            return (employee.FirstOrDefault(m => m.EmaiLId   == EmailId && m.Password == Password));
 
         }
     }
